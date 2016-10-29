ca0cb2e441f50462755fbe6af5d815b781d59088
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2016-10-29 10:03:00 +0300

    fix typo

--- src/KnxConnectionFSM.js
+++ src/KnxConnectionFSM.js
@@ -167,7 +167,7 @@ module.exports = machina.Fsm.extend({
           default:
             this.debugPrint(util.format(
               '*** error *** (connstate.code: %d)', datagram.connstate.status));
-            this.emit('error', KNXConstants.keyText('RESPONSECODE', datagram.connstate.status));
+            this.emit('error', KnxConstants.keyText('RESPONSECODE', datagram.connstate.status));
             this.transition( 'connecting' );
         }
       },
