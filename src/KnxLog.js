ce76fba134c31a77d5d1d76f67c8d7a12f2b3a7e
qm3ster <qm3ster@gmail.com>
2020-11-05 13:41:11 +0200

    Clean up KnxLog.js

--- src/KnxLog.js
+++ src/KnxLog.js
@@ -1,34 +1,28 @@
 /**
-* knx.js - a KNX protocol stack in pure Javascript
-* (C) 2016-2018 Elias Karakoulakis
-*/
+ * knx.js - a KNX protocol stack in pure Javascript
+ * (C) 2016-2018 Elias Karakoulakis
+ */
 const util = require('util');
-var logger;
+let logger;
+
+const create = (options) => {
+  const level =
+    (options && (options.debug ? 'debug' : options.loglevel)) || 'info';
+  //console.trace('new logger, level='+lvl);
+  return require('log-driver')({
+    level,
+    format(lvl, msg /*string*/, ...a) {
+      // lvl is the log level ie 'debug'
+      const ts = new Date().toISOString().replace(/T/, ' ').replace(/Z$/, '');
+      return a.length
+        ? // if more than one item to log, assume msg is a fmt string
+          util.format('[%s] %s ' + msg, lvl, ts, ...a)
+        : // otherwise, msg is a plain string
+          util.format('[%s] %s %s', lvl, ts, msg);
+    },
+  });
+};
 
 module.exports = {
-  get: function(options) {
-    if (!logger) {
-      lvl = (options && options.debug && 'debug') ||
-          (options && options.loglevel) ||
-          'info';
-      //console.trace('new logger, level='+lvl);
-      logger = require('log-driver')({
-        level: lvl,
-        format: function() {
-          // arguments[0] is the log level ie 'debug'
-          var a  = Array.from(arguments);
-          var ts = new Date().toISOString().replace(/T/, ' ').replace(/Z$/, '');
-          if (a.length > 2) {
-            // if more than one item to log, assume a fmt string is given
-            var fmtargs = ['[%s] %s '+a[1], a[0], ts].concat(a.slice(2));
-            return util.format.apply(util, fmtargs);
-          } else {
-            // arguments[1] is a plain string
-            return util.format('[%s] %s %s', a[0], ts, a[1])
-          }
-        }
-      });
-    }
-    return(logger);
-  }
+  get: (options) => logger || (logger = create(options)),
 };
